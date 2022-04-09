83c80a98f23ab2ee12aeab66f5771cbafa6751e3
Anderson Martins <anderson.martins@simply.com.br>
2022-04-08 21:02:25 -0300

    Faz ajustes no componente LetterBox
    Ajusta o componente pra que mostre o resultado somente
    caso a resposta esteja correta

--- src/components/LetterBox.vue
+++ src/components/LetterBox.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="results"
-    v-for="(result, index) in results" :key="index">
+  <div class="results" v-for="(result, index) in results" :key="index">
     <div
       class="try"
       v-for="(letter, i) in [...word]"
       :key="i"
-      :class="results[index][i]"
-    >{{ allCorrect ? letter : '' }}
+      :class="result.status[i]"
+    >
+      <span class="letter">{{ result.allCorrect ? letter : "" }}</span>
     </div>
   </div>
   <div class="try-box">
     <input type="text" v-model="teste" />
     <button @click="verify">Verificar</button>
+  </div>
+  <div>
+    <span class="error" v-for="(error, index) in errors" :key=index>{{error}}</span>
   </div>
 </template>
 
@@ -20,35 +23,42 @@ export default {
   data() {
     return {
       word: "teste",
-      teste: "tetsa",
-      status: [],
-      results:[],
-      allCorrect: false
+      teste: "teste",
+      results: [
+        {
+          status: [],
+          allCorrect: false,
+        },
+      ],
+      errors: new Set()
     };
   },
   methods: {
     verify() {
-      this.status = [];
+      var result = { status: [], allCorrect: false };
+
       for (var i = 0; i < this.teste.length; i++) {
         if (this.teste[i] == this.word[i]) {
-          this.status.push("correct");
-          this.allCorrect = true
+          result.status.push("correct");
+          result.allCorrect = true;
         } else if (this.word.includes(this.teste[i])) {
-          this.status.push("outOfPlace");
-          this.allCorrect = false
+          result.status.push("outOfPlace");
+          result.allCorrect = false;
         } else {
-          this.status.push("incorrect");
-          this.allCorrect = false
+          result.status.push("incorrect");
+          result.allCorrect = false;
+
+          this.errors.add(this.teste[i])
         }
       }
-      this.results.push(this.status)
+
+      this.results.push(result);
     },
   },
 };
 </script>
 
 <style>
-
 .try {
   display: inline-block;
   width: 40px;
@@ -65,9 +75,25 @@ export default {
   margin: 2px;
 }
 
+.letter {
+  font-size: 20px;
+  font-weight: bold;
+  display: flex;
+  /* align-self: center; */
+  justify-content: center;
+  padding: 10px;
+  text-transform: capitalize;
+}
+
+.error {
+  font-size: 15px;
+  font-weight: bold;
+  color: red;
+}
+
 .results {
   display: block;
-  margin-top: 5px;
+  margin-top: 4px;
 }
 
 .try-box {
